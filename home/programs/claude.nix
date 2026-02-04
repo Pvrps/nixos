9b1e001772c6bb4126b516a72df284a3cbd85480
purps <github@purps.ca>
2026-02-04 12:40:05 -0500

    Update

--- home/programs/claude.nix
+++ home/programs/claude.nix
@@ -3,4 +3,13 @@
   home.packages = [
     inputs.claude-code.packages.${pkgs.system}.default
   ];
+
+  home.sessionVariables = {
+    ANTHROPIC_BASE_URL = "http://127.0.0.1:4141";
+    ANTHROPIC_API_KEY = "copilot-api";
+    ANTHROPIC_MODEL = "gpt-5-mini";
+
+    CLAUDE_NO_TELEMETRY = "1";
+    CLAUDE_AUTO_UPDATE = "0";
+  };
 }
