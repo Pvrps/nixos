37b2742b3069a2f34ec497b965a2a13f601eece0
purps <github@purps.ca>
2026-02-03 19:04:45 -0500

    Update

--- home/programs/steam.nix
+++ home/programs/steam.nix
@@ -1,23 +1,46 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
     
   ];
 
+  xdg.desktopEntries.steam = {
+    name = "Steam";
+    genericName = "Application Distribution Platform";
+    #exec = "env MANGOHUD=1 steam %U";
+    exec = "steam %U";
+    icon = "steam";
+    terminal = false;
+    categories = [ "Network" "FileTransfer" "Game" ];
+    mimeType = [ "x-scheme-handler/steam" "x-scheme-handler/steamlink" ];
+  };
+
   programs.mangohud = {
     enable = true;
     settings = {
-      full = true;
-      no_display = true;
-      #cpu_temp = true;
-      #gpu_temp = true;
-      #ram = true;
-      #vram = true;
-      fps = true;
-      #frametime = true;
-
+      round_corners = 20;
+      text_outline = false;
+      font_size = lib.mkForce 24;
+      
       position = "top-right";
-      width = 280;
+      table_columns = 3;
+      cell_padding_vertical = 8;
+
+      gpu_stats = true;
+      gpu_temp = true;
+      
+      cpu_stats = true;
+      cpu_temp = true;
+
+      ram = true;
+      vram = true;
+
+      fps = true;
+      frametime = false;
+      frame_timing = 1;
+      
+      histogram_height = 24;
     };
   };
+
 }
