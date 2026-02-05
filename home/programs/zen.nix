104d8b7f668ef15fdf1f6a9424b1c052ba947e8c
purps <github@purps.ca>
2026-02-04 19:04:32 -0500

    Update

--- home/programs/zen.nix
+++ home/programs/zen.nix
@@ -4,11 +4,31 @@
     inputs.zen-browser.homeModules.twilight
   ];
 
-  stylix.targets.zen-browser.profileNames = [ "default" ];
+  stylix.targets.zen-browser.profileNames = [ "Personal" ];
 
   programs.zen-browser = {
     enable = true;
+    languagePacks = [ "en-US" ];
 
+    profiles.Personal = {
+      id = 0;
+      name = "Personal";
+      isDefault = true;
+      
+      mods = [
+        "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
+        "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
+        "c6813222-6571-4ba6-8faf-58f3343324f6" # Disable Rounded Corners
+        "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
+        "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+        "cb15abdb-0514-4e09-8ce5-722cf1f4a20f" # Hide Extension Name
+        "803c7895-b39b-458e-84f8-a521f4d7a064" # Hide Inactive Workspaces
+        "4ab93b88-151c-451b-a1b7-a1e0e28fa7f8" # No Sidebar Scrollbar
+        "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        "c8d9e6e6-e702-4e15-8972-3596e57cf398" # Zen Back Forward
+      ];
+    };
+        
     policies = {
       AutofillAddressEnabled = false;
       AutofillCreditCardEnabled = false;
@@ -29,7 +49,7 @@
         Cryptomining = true;
         Fingerprinting = true;
       };
-
+      
       FirefoxHome = {
         Search = true;
         Pocket = false;
@@ -39,23 +59,9 @@
         SponsoredPocket = false;
         SponsoredTopSites = false;
       };
-
+      
       SearchSuggestEnabled = true;
       DefaultSearchEngine = "Google";
-
-      Preferences = {
-        "browser.search.suggest.enabled" = true;
-        "browser.urlbar.suggest.searches" = true;
-        "browser.urlbar.showSearchSuggestionsFirst" = true;
-
-        "ui.systemUsesDarkTheme" = 1;
-        "zen.theme.dark" = true;
-        "layout.css.prefers-color-scheme.content-override" = 0;
-
-        "zen.view.compact-mode" = true;
-        "zen.view.compact.hide-toolbar" = false;
-        "zen.view.compact.hide-sidebar" = false;
-      };
 
       ExtensionSettings = {
         "*" = {
@@ -64,24 +70,20 @@
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
-          pinned = true;
         };
         "sponsorBlocker@ajay.app" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
           installation_mode = "force_installed";
-          pinned = false;
         };
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
-          pinned = true;
         };
         "enhancerforyoutube@maximerf.addons.mozilla.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/enhancer-for-youtube/latest.xpi";
           installation_mode = "force_installed";
-          pinned = false;
         };
       };
-    };   
+    };
   };
 }
