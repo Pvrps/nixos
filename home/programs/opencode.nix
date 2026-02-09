d42c6e0a4d62a66b8af1c63f074247b5ae963b2a
purps <github@purps.ca>
2026-02-09 11:03:21 -0500

    feat: update flake inputs and opencode config

--- home/programs/opencode.nix
+++ home/programs/opencode.nix
@@ -2,9 +2,21 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  opencodePkg = inputs.opencode.packages.${pkgs.system}.default;
+
+  # TODO: Remove this override once upstream issue #12817 is resolved
+  # https://github.com/anomalyco/opencode/issues/12817
+  fixedNodeModules = opencodePkg.node_modules.overrideAttrs (old: {
+    outputHash = "sha256-fPXBw/ZBo2J8kIjgfVh5cwBfMRQWOpBH7djHncnSdpA=";
+  });
+
+  fixedOpencode = opencodePkg.override {
+    node_modules = fixedNodeModules;
+  };
+in {
   home.packages = [
-    inputs.opencode.packages.${pkgs.system}.default
+    fixedOpencode
   ];
 
   home.file.".config/opencode/opencode.json".text = builtins.toJSON {
