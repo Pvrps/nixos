dbde3e63d114cb1fe1b344d75983469471ca41ed
purps <github@purps.ca>
2026-02-10 08:27:04 -0500

    Update

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -113,7 +113,17 @@
 
   nixpkgs.config.allowUnfree = true;
   nix = {
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://mic92.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "mic92.cachix.org-1:vL3/V4C9t6xBFZ8c4hA29EfGaKjXcruzjxj815V/V24="
+      ];
+    };
     gc = {
       automatic = true;
       dates = "weekly";
