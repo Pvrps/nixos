ba67a595e11d5d5f18cd973fac5ff2addcebcec1
purps <github@purps.ca>
2026-02-20 10:49:50 -0500

    feat(fish): update fish shell aliases/functions

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -4,5 +4,12 @@ _: {
     interactiveShellInit = ''
       set fish_greeting
     '';
+
+    functions = {
+      run = ''
+        NIXPKGS_ALLOW_UNFREE=1 nix-shell -p steam-run --run "steam-run $argv"
+      '';
+    };
+
   };
 }
