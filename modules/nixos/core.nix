64f4ae79f54607b37d3aed62c498bcfe892bacb9
purps <github@purps.ca>
2026-03-22 11:53:56 -0400

    feat(boot): hide boot stuff and add plymouth

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -7,6 +7,21 @@
       };
       efi.canTouchEfiVariables = true;
     };
+    plymouth = {
+      enable = true;
+      theme = "bgrt"; # Manufacturer logo with a spinner
+    };
+    consoleLogLevel = 0; # Hide kernel output during boot
+    initrd.verbose = false; # Hide initrd output
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 
   networking.hostName = "desktop";
