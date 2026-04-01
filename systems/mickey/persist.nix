bcfe8aa42b84f89f6bf1740d4454e48a3c3e33f4
purps <github@purps.ca>
2026-04-01 10:48:20 -0400

    fix(mickey): persist ssh host key files individually to prevent sshd_config masking

--- systems/mickey/persist.nix
+++ systems/mickey/persist.nix
@@ -6,7 +6,6 @@
     hideMounts = true;
 
     directories = [
-      "/etc/ssh"
       "/var/log"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
@@ -18,6 +17,10 @@
 
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
 
     users = {
