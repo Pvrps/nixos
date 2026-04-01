e191dffbb261c71b45b017089c93e5b6f469aeb8
purps <github@purps.ca>
2026-04-01 12:14:01 -0400

    fix(mickey): downgrade to i965 intel driver to prevent krdp hardware encoding crash

--- systems/mickey/hardware.nix
+++ systems/mickey/hardware.nix
@@ -16,8 +16,7 @@
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (Fall back to older, more stable driver)
       libvdpau-va-gl
     ];
   };
