a07b656784eca0e5bd5c47066070c91bfccd1848
purps <github@purps.ca>
2026-04-04 22:32:22 -0400

    Add github PAT

--- systems/desktop/users.nix
+++ systems/desktop/users.nix
@@ -30,6 +30,11 @@
         group = "users";
         mode = "0600";
       };
+      "github-token" = {
+        owner = "purps";
+        group = "users";
+        mode = "0600";
+      };
       "context7-api-key" = {
         owner = "purps";
         group = "users";
