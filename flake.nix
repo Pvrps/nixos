13246fcebce017306bb06c97024c3535f346db92
purps <github@purps.ca>
2026-04-10 11:02:41 -0400

    flake.nix: use latest tag for lix-module, remove unnecessary lix input

--- flake.nix
+++ flake.nix
@@ -78,15 +78,9 @@
       url = "github:vic/import-tree";
     };
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/latest.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
     };
   };
 
@@ -100,7 +94,6 @@
     nix-flatpak,
     import-tree,
     lix-module,
-    lix,
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
