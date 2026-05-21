5d759b9255368d8a02c6a85609cc1fc73637d8e1
purps <github@purps.ca>
2026-05-20 20:22:44 -0400

    perf: wrap osu! with gamemoderun via overridden .desktop entry

--- modules/home/programs/osulazer.nix
+++ modules/home/programs/osulazer.nix
@@ -14,6 +14,25 @@ in {
     # Just the native package, no wrappers needed
     home.packages = [pkgs.osu-lazer-bin];
 
+    # Override the stock .desktop entry to launch via gamemoderun (applies CPU EPP + niceness)
+    # The entry name "osu!" matches the package's desktop file name, replacing it in XDG
+    xdg.desktopEntries."osu!" = {
+      name = "osu!";
+      comment = "A free-to-win rhythm game. Rhythm is just a *click* away!";
+      exec = "gamemoderun osu! %u";
+      icon = "osu";
+      terminal = false;
+      categories = ["Game"];
+      mimeType = [
+        "application/x-osu-beatmap-archive"
+        "application/x-osu-skin-archive"
+        "application/x-osu-beatmap"
+        "application/x-osu-storyboard"
+        "application/x-osu-replay"
+        "x-scheme-handler/osu"
+      ];
+    };
+
     custom.programs.niri.windowRules = lib.mkIf config.custom.programs.niri.enable [
       ''
         window-rule {
