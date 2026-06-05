a49694deb5a415a824a05ee99492ba79b081878c
purps <github@purps.ca>
2026-06-04 21:16:43 -0400

    Fix immich upload dir

--- modules/hosts/windwaker/services/immich.nix
+++ modules/hosts/windwaker/services/immich.nix
@@ -2,12 +2,10 @@
   config,
   lib,
   ...
-}:
-let
+}: let
   dockerVolumeDir = "/mnt/general/docker";
   uploadVolumeDir = "/mnt/media/docker";
-in
-{
+in {
   sops.secrets."immich-env".sopsFile = ./_secrets.yaml;
 
   virtualisation.oci-containers.containers = {
@@ -22,9 +20,9 @@ in
         "redis"
         "database"
       ];
-      ports = [ "62283:2283" ];
+      ports = ["62283:2283"];
       volumes = [
-        "${dockerVolumeDir}/immich/library:/usr/src/app/upload"
+        "${uploadVolumeDir}/immich/library:/usr/src/app/upload"
         "/etc/localtime:/etc/localtime:ro"
       ];
     };
@@ -32,21 +30,21 @@ in
     immich-machine-learning = {
       image = "ghcr.io/immich-app/immich-machine-learning:release";
       autoStart = true;
-      networks = [ "immich_internal" ];
-      volumes = [ "model-cache:/cache" ];
+      networks = ["immich_internal"];
+      volumes = ["model-cache:/cache"];
     };
 
     redis = {
       image = "docker.io/valkey/valkey:8-bookworm@sha256:fec42f399876eb6faf9e008570597741c87ff7662a54185593e74b09ce83d177";
       autoStart = true;
-      networks = [ "immich_internal" ];
+      networks = ["immich_internal"];
     };
 
     database = {
       image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0";
       autoStart = true;
-      networks = [ "immich_internal" ];
-      environmentFiles = [ config.sops.secrets."immich-env".path ];
+      networks = ["immich_internal"];
+      environmentFiles = [config.sops.secrets."immich-env".path];
       environment = {
         POSTGRES_INITDB_ARGS = "--data-checksums";
         DB_STORAGE_TYPE = "HDD";
@@ -59,24 +57,24 @@ in
 
   systemd.services = {
     "docker-immich-server" = {
-      after = [ "docker-networks.service" ];
-      requires = [ "docker-networks.service" ];
-      bindsTo = [ "docker.service" ];
+      after = ["docker-networks.service"];
+      requires = ["docker-networks.service"];
+      bindsTo = ["docker.service"];
     };
     "docker-immich-machine-learning" = {
-      after = [ "docker-networks.service" ];
-      requires = [ "docker-networks.service" ];
-      bindsTo = [ "docker.service" ];
+      after = ["docker-networks.service"];
+      requires = ["docker-networks.service"];
+      bindsTo = ["docker.service"];
     };
     "docker-redis" = {
-      after = [ "docker-networks.service" ];
-      requires = [ "docker-networks.service" ];
-      bindsTo = [ "docker.service" ];
+      after = ["docker-networks.service"];
+      requires = ["docker-networks.service"];
+      bindsTo = ["docker.service"];
     };
     "docker-database" = {
-      after = [ "docker-networks.service" ];
-      requires = [ "docker-networks.service" ];
-      bindsTo = [ "docker.service" ];
+      after = ["docker-networks.service"];
+      requires = ["docker-networks.service"];
+      bindsTo = ["docker.service"];
     };
   };
 }
