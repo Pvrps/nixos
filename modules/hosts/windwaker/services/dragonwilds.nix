23d5b16da6097e110b8bb04fe366cce1b189cc94
purps <github@purps.ca>
2026-06-06 09:10:27 -0400

    fix(windwaker): expose dragonwilds port 7777/udp to host

--- modules/hosts/windwaker/services/dragonwilds.nix
+++ modules/hosts/windwaker/services/dragonwilds.nix
@@ -13,6 +13,7 @@ in
       image = "indifferentbroccoli/runescape-dragonwilds-server-docker:latest";
       autoStart = true;
       networks = [ "lan_bridge" ];
+      ports = [ "7777:7777/udp" ];
       environmentFiles = [ config.sops.secrets."dragonwilds-env".path ];
       environment = {
         OWNER_ID = "0002ae80ae5c49c684eb9fdd41395eb7";
