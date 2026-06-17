2072424c4323c9a7b4ca01a5ee3154b2feeb28d5
purps <github@purps.ca>
2026-06-16 21:55:53 -0400

    Fix audiobook mount

--- modules/hosts/windwaker/services/audiobookshelf.nix
+++ modules/hosts/windwaker/services/audiobookshelf.nix
@@ -6,7 +6,7 @@ in {
       image = "ghcr.io/advplyr/audiobookshelf:latest";
       publishPorts = ["13378:80"];
       volumes = [
-        "${dockerVolumeDir}/audiobookshelf/audiobooks:/audiobooks"
+        "${dockerVolumeDir}/audiobookshelf/audiobooks:/app/Library"
         "${dockerVolumeDir}/audiobookshelf/podcasts:/podcasts"
         "${dockerVolumeDir}/audiobookshelf/config:/config"
         "${dockerVolumeDir}/audiobookshelf/metadata:/metadata"
